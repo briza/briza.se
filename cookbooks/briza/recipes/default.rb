7a0125eaa2494eb346d220070c4698b65c8bde89
Morgan Roderick <morgan@roderick.dk>
2011-11-05 15:54:13 +0100

    Making the rvm installed Ruby 1.9.x treat all strings as UTF-8, which will prevent exceptions when Jekyll generates site

--- cookbooks/briza/recipes/default.rb
+++ cookbooks/briza/recipes/default.rb
@@ -1,3 +1,19 @@
+directory "/etc/profile.d" do
+    action :create
+    owner "root"
+    group "root"
+    mode 0755
+  end
+
+# make sure that Ruby interprets strings as UTF-8 by default, which makes 1.9 not generate exceptions for
+# Scandinavian characters in UTF-8 strings
+cookbook_file "/etc/profile.d/set_utf8_default_ruby_string.sh" do
+    source "set_utf8_default_ruby_string.sh"
+    mode 0644
+    owner "root"
+    group "root"
+end
+
 require_recipe "rvm::vagrant"
 require_recipe "rvm::system"
 
